9889fbfd97978e6c800a70a8aef344537c155aca
Vikas Soni <165142441+Vikasgits@users.noreply.github.com>
2024-04-17 19:41:18 +0530

    Add files via upload

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Dashboard() {
+  return (
+    <div className="flex flex-1 justify-center items-center text-white text-3xl h-full">
+      Welcome to Studynotion
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,18 @@
+import Template from "../components/Template";
+import signupImg from "../assets/signup.png";
+import React from 'react'
+
+function Signup({ setIsLoggedIn }) {
+  return (
+    <Template
+      title="Join the millions learning to code with StudyNotion for free"
+      description1="Build skills for today, tomorrow, and beyond."
+      description2="Education to future-proof your career."
+      image={signupImg}
+      formtype="signup"
+      setIsLoggedIn={setIsLoggedIn}
+    />
+  );
+}
+
+export default Signup;
